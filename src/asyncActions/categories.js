4406ba50c20c7075b01c25244a8a2b9bda0d214f
Oleg Khutorianskiy <compilated510@outlook.com>
2023-05-18 08:20:01 +0200

    Delete Async action catalog

--- src/asyncActions/categories.js
+++
@@ -1,8 +0,0 @@
-import { categoriesLoadAction } from "../store/reducers/categoriesReducer";
-
-
-export const asyncloadCategoriesAction = async(dispatch) => {
-    const responce = await fetch('http://localhost:3333/categories/all')
-    const data = await responce.json();
-    dispatch(categoriesLoadAction(data));
-}
